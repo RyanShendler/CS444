165aa72f0c70efc2efd84296d3f2ed93c005fbbf
Ryan Shendler <rshendl1@CS544-rshendl1.pods.bu.int>
2021-12-02 13:23:34 -0500

    finished statement

---
+++ submit/prj5-sol/src/index.mjs
@@ -0,0 +1,34 @@
+import makeAccountsAppElement from './accounts-app.mjs'; 
+import makeWs from './ws.mjs';
+import makeServices from './accounts-services.mjs';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './react/app.jsx';
+
+const DEFAULT_WS_URL = 'https://zdu.binghamton.edu:2345';
+
+
+/** Return url set in query param 'ws-url' if present; otherwise
+ *  return DEFAULT_WS_URL.
+ */
+function getWsUrl() {
+  const locationUrl = new URL(window.location.href);
+  return locationUrl.searchParams.get('ws-url') ?? DEFAULT_WS_URL;
+}
+
+function main() {
+  const ws = makeWs(getWsUrl());
+  const services = makeServices(ws);
+  const extendFn = (accountId, element, updateAccountDetails) => {
+    const props = {services, ws, accountId, updateAccountDetails};
+    const app = React.createElement(App, props);
+    ReactDom.render(app, element);
+  };
+  makeAccountsAppElement(services, ws, extendFn);
+}
+
+main();
+
+
---
+++ submit/prj5-sol/src/react/act-add.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { reportErrors } from './util.jsx';
+
+/** Display form containing date, memo and amount widgets along with a
+ *  submit button.  When the form is submitted, submit the form data
+ *  to props.services.newAccount().  If the response contains errors
+ *  then display form-errors before the form, widget errors below each
+ *  widget.  If the response does not contain any errors, then update
+ *  the *Account Detail* display by calling
+ *  `props.updateAccountDetails()` to redisplay the *Account Detail*
+ *  with an updated balance.
+ */
+export default function(props) {
+  return 'TODO Transaction Add'
+}
